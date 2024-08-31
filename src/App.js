141343b76007fc69e793d31dcb1de9da92fd24cb
Brandon <112407387+bholmdev@users.noreply.github.com>
2024-08-31 16:54:40 -0500

    week 1 workshop complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import CampsitesList from './features/campsites/CampsitesList';
-import  { Container, Navbar, NavbarBrand }  from "reactstrap";
-import NucampLogo from "./app/assets/img/logo.png";
 import './App.css';
+import Header from './components/Header';
+import CampsitesList from './features/campsites/CampsitesList';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
         <div className="App">
-            <Navbar dark color="primary" sticky="top" expand="md">
-                <Container>
-                    <NavbarBrand href="/">
-                        <img src={NucampLogo} alt="nucamp logo"></img>
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
+            <Header />
             <CampsitesList />
+            <Footer />
         </div>
     );
 }
