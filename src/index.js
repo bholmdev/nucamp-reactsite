021ed6db1dcbdad2e0b52f3dab63d60f119582f0
Brandon <112407387+bholmdev@users.noreply.github.com>
2024-09-02 18:58:09 -0500

    changed basename in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const root = createRoot(container);
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename="/nucamp-reactsite">
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <App />
             </BrowserRouter>
             
